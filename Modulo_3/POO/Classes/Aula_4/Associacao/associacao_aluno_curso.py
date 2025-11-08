c3fedb48eb7b814da76f9b349c8740806e73ddda
André Borges <andreborges22@gmail.com>
2025-11-07 22:55:37 -0300

    incluindo tratamento de excecao

--- Modulo_3/POO/Classes/Aula_4/Associacao/associacao_aluno_curso.py
+++ Modulo_3/POO/Classes/Aula_4/Associacao/associacao_aluno_curso.py
@@ -48,55 +48,58 @@ class Curso:
 
     def __str__(self):
         return f"Curso: {self.nome}"
-    
+
+try:    
     # --- Exemplo de uso ---
 
-# Criando alunos
-a1 = Aluno("Ana")
-a2 = Aluno("Carlos")
-a3 = Aluno("João")
+    # Criando alunos
+    a1 = Aluno("Ana")
+    a2 = Aluno("Carlos")
+    a3 = Aluno("João")
 
-# Criando cursos
-c1 = Curso("Python Básico")
-c2 = Curso("Banco de Dados")
+    # Criando cursos
+    c1 = Curso("Python Básico")
+    c2 = Curso("Banco de Dados")
 
-# Associação (Aluno  Curso)
-a1.inscrever(c1)
-a1.inscrever(c2)
-a2.inscrever(c1)
-a2.inscrever(c2)
-a3.inscrever(c1)
-a3.inscrever(c2)
+    # Associação (Aluno  Curso)
+    a1.inscrever(c1)
+    a1.inscrever(c2)
+    a2.inscrever(c1)
+    a2.inscrever(c2)
+    a3.inscrever(c1)
+    a3.inscrever(c2)
 
+    # Exibindo as associações
+    print(f"\nCursos de {a1.nome}: {[curso.nome for curso in a1.cursos]}")
+    '''for curso in a1.cursos:
+        print(curso.nome)'''
+    print(f"Cursos de {a2.nome}: {[curso.nome for curso in a2.cursos]}")
+    print(f"Cursos de {a3.nome}: {[curso.nome for curso in a3.cursos]}")
 
-# Exibindo as associações
-print(f"\nCursos de {a1.nome}: {[curso.nome for curso in a1.cursos]}")
-'''for curso in a1.cursos:
-    print(curso.nome)'''
-print(f"Cursos de {a2.nome}: {[curso.nome for curso in a2.cursos]}")
-print(f"Cursos de {a3.nome}: {[curso.nome for curso in a3.cursos]}")
+    print(f"\nAlunos do curso {c1.nome}: {[aluno.nome for aluno in c1.alunos]}")
+    print(f"Alunos do curso {c2.nome}: {[aluno.nome for aluno in c2.alunos]}")
 
-print(f"\nAlunos do curso {c1.nome}: {[aluno.nome for aluno in c1.alunos]}")
-print(f"Alunos do curso {c2.nome}: {[aluno.nome for aluno in c2.alunos]}")
+    # Demonstrando independência
+    print("\n--- Exemplo de Independência ---")
+    print("Se o aluno Ana (a1) desistir, o curso Python Básico (c1) continua existindo.")
+    print("rodando o comando del a1")
+    #a1.cancelar()
+    del a1  # removendo o objeto 'Ana'
+    print("Curso ainda existe:", c1.nome)
+    print("Curso ainda existe:", c2.nome)
+    print(f"\nAlunos do curso {c1.nome}: {[aluno.nome for aluno in c1.alunos]}")
+    print(f"Alunos do curso {c2.nome}: {[aluno.nome for aluno in c2.alunos]}")
 
-# Demonstrando independência
-print("\n--- Exemplo de Independência ---")
-print("Se o aluno Ana (a1) desistir, o curso Python Básico (c1) continua existindo.")
-print("rodando o comando del a1")
-#a1.cancelar()
-del a1  # removendo o objeto 'Ana'
-print("Curso ainda existe:", c1.nome)
-print("Curso ainda existe:", c2.nome)
-print(f"\nAlunos do curso {c1.nome}: {[aluno.nome for aluno in c1.alunos]}")
-print(f"Alunos do curso {c2.nome}: {[aluno.nome for aluno in c2.alunos]}")
+    # Demonstrando independência
+    print("\n--- Exemplo de Independência ---")
+    print("Se o curso Banco de Dados (c2) terminar, O aluno Carlos (a2) ainda existe.")
+    print("rodando o comando del c2")
+    #c2.cancelar()
+    del c2  # removendo o objeto 'Banco de Dados'
+    print("Aluno ainda existe:", a2.nome)
+    print("Aluno ainda existe:", a3.nome)
+    print(f"Cursos de {a2.nome}: {[curso.nome for curso in a2.cursos]}")
+    print(f"Cursos de {a3.nome}: {[curso.nome for curso in a3.cursos]}")
 
-# Demonstrando independência
-print("\n--- Exemplo de Independência ---")
-print("Se o curso Banco de Dados (c2) terminar, O aluno Carlos (a2) ainda existe.")
-print("rodando o comando del c2")
-#c2.cancelar()
-del c2  # removendo o objeto 'Banco de Dados'
-print("Aluno ainda existe:", a2.nome)
-print("Aluno ainda existe:", a3.nome)
-print(f"Cursos de {a2.nome}: {[curso.nome for curso in a2.cursos]}")
-print(f"Cursos de {a3.nome}: {[curso.nome for curso in a3.cursos]}")
+except Exception as e:
+    print(f"Erro: {e}")
