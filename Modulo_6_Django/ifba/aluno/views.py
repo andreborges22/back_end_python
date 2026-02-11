210fb0083876f410c0413bef9c62b3a80a9c100f
André Borges <andreborges@INTRAUECA.CEFET.COM>
2026-02-11 16:00:27 -0300

    listando alunos na home do aluno

--- Modulo_6_Django/ifba/aluno/views.py
+++ Modulo_6_Django/ifba/aluno/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Aluno
 
-# Create your views here.
+
 def home(request):
     alunos = Aluno.objects.all()
     return render(request, 'index.html',{"alunos":alunos})
--- Modulo_6_Django/ifba/views.py
+++ Modulo_6_Django/ifba/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
+from .models import Aluno
 
-def home(request):
-    return render(request,"index.html")
+# Create your views here.
