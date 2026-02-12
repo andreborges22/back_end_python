655d90d9d3afe142aa4cab13279af093992f6613
andreborges22 <andreborges22@gmail.com>
2026-02-11 21:50:13 -0300

    Alterações na index de aluno

--- Modulo_6_Django/ifba/aluno/models.py
+++ Modulo_6_Django/ifba/aluno/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 # Create your models here.
-class Aluno(models.Model):    
+
+
+class Aluno(models.Model):
     nome = models.CharField(max_length=100)
 
-    def __init__(self):
-        return super().__str__()
+    def __str__(self):
+         return self.nome
