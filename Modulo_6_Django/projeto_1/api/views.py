c1fd2136017c6ba8e58ac5b1bb2baff4862a3d5e
André Borges <andreborges@INTRAUECA.CEFET.COM>
2026-02-24 15:22:00 -0300

    integracao do crud django com django rest framwork. foi adicionado oao projeto o app api contendo o serializer e o viewset

--- Modulo_6_Django/projeto_1/api/views.py
+++ Modulo_6_Django/projeto_1/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from aluno.models import Aluno
+from .serializers import AlunoSerializer
 
 # Create your views here.
+class AlunoViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
